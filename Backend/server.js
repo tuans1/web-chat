b3a703ebf1acaf99d00948777ee4a43a21ac6fb6
nguyen.toan <nguyen.toan@ascendcorp.com>
2023-03-28 00:05:24 +0700

    update socket BE

--- Backend/server.js
+++ Backend/server.js
@@ -4,32 +4,15 @@ const route = require("./src/routes/index");
 const database = require("./src/config/database");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const server = require("http").createServer(app);
-const WebSocket = require("ws");
-
-const wss = new WebSocket.Server({ server: server });
-
-// wss.on("connection", function connection(ws) {
-//   console.log("A new client Connected!");
-//   ws.send("Welcome New Client!");
-
-//   ws.on("message", function incoming(message) {
-//     console.log("received: %s", message);
-    
-//     // wss.clients.forEach(function each(client) {
-//     //   console.log(client);
-//     //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-//     //     client.send(message);
-//     //   }
-//     // });
-//   });
-// });
-
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const SocketServices = require("./src/services/SocketService");
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(route);
 database.connect();
-
-server.listen(5000);
+global._io = io; // cach 2
+global._io.on("connection", SocketServices.connection);
+app.listen(5000);
--- Backend/src/routes/message.js
+++ Backend/src/routes/message.js
@@ -7,25 +7,11 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server: server });
 // middleware that is specific to this router
 // define the home page route
-wss.on("connection", function connection(ws) {
-  console.log("A new client Connected!");
-  ws.send("Welcome New Client!");
-
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-    
-    // wss.clients.forEach(function each(client) {
-    //   console.log(client);
-    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //     client.send(message);
-    //   }
-    // });
-  });
-});
 router.use((req, res, next) => {
   console.log("Time in ROOM: ", Date.now());
   next();
 });
+
 router.get("/test",(req,res)=>{
   
 });
